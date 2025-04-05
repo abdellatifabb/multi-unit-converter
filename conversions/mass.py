6aeed913f1826071b4a64733ddf8afa0782a7a08
abdellatif <abdellatiferrazouqi@gmail.com>
2025-04-05 11:04:07 +0000

    Kg to LB

---
+++ conversions/mass.py
@@ -0,0 +1,18 @@
+"""
+Module: mass.py
+Provides functions for converting mass between kilograms and pounds.
+"""
+
+
+
+def kg_to_lb(kg):
+    """
+    Convert kilograms to pounds.
+
+    Args:
+        kg (float): Mass in kilograms.
+
+    Returns:
+        float: Mass in pounds.
+    """
+    return kg * 2.20462
